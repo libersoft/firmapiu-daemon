e74e029609f54058b5ce49085e1e658c2124d179
dellanna <ada@libersoft.it>
2015-04-30 16:55:52 +0200

    Adeguamento demone alle nuove firme di libfirmapiu-java

--- src/main/java/it/libersoft/firmapiud/dbusinterface/FirmapiuDImpl.java
+++ src/main/java/it/libersoft/firmapiud/dbusinterface/FirmapiuDImpl.java
@@ -4,15 +4,24 @@
 package it.libersoft.firmapiud.dbusinterface;
 
 import it.libersoft.firmapiu.Data;
-import it.libersoft.firmapiu.DataFilePath;
-import it.libersoft.firmapiu.GenericArgument;
-import it.libersoft.firmapiu.MasterFactoryBuilder;
-import it.libersoft.firmapiu.cades.CadesBESCommandInterface;
+import it.libersoft.firmapiu.ResultInterface;
+//import it.libersoft.firmapiu.DataFilePath;
+//import it.libersoft.firmapiu.GenericArgument;
+//import it.libersoft.firmapiu.MasterFactoryBuilder;
+//import it.libersoft.firmapiu.cades.CadesBESCommandInterface;
+import it.libersoft.firmapiu.cades.CadesBESFactory;
+import it.libersoft.firmapiu.cades.P7FileCommandInterface;
 import it.libersoft.firmapiu.exception.FirmapiuException;
 import it.libersoft.firmapiu.cades.CommandProxyInterface;
+import it.libersoft.firmapiu.crtoken.PKCS11Token;
+import it.libersoft.firmapiu.crtoken.TokenFactoryBuilder;
+import it.libersoft.firmapiu.data.DataFactoryBuilder;
+import it.libersoft.firmapiu.data.DataFile;
 import static it.libersoft.firmapiu.consts.FactoryConsts.*;
 import static it.libersoft.firmapiu.consts.FactoryPropConsts.*;
+import static it.libersoft.firmapiu.consts.ArgumentConsts.*;
 
+import java.io.File;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
@@ -32,7 +41,8 @@ public final class FirmapiuDImpl implements FirmapiuDInterface {
 
 	//FIXME da cambiare nel momento in cui si riscrive libreria
 	private final CommandProxyInterface cmdInterface;
-	private final CadesBESCommandInterface cadesBesInterface;
+	//interfaccia di comandi specializzata per la gestione di file .p7m .p7s
+	//private final P7FileCommandInterface p7CommandInterface;
 	//resource bundle di FirmapiuD
 	private final ResourceBundle localrb;
 	
@@ -47,7 +57,7 @@ public final class FirmapiuDImpl implements FirmapiuDInterface {
 		this.localrb = ResourceBundle.getBundle("it.libersoft.firmapiud.lang.locale",Locale.getDefault());
 		//FIXME da cambiare nel momento in cui si riscrive libreria
 		this.cmdInterface=new CommandProxyInterface(rb);
-		cadesBesInterface = MasterFactoryBuilder.getFactory(CADESBESFACTORY).getCadesBESCommandInterface(P7MFILE);
+		//this.p7CommandInterface = CadesBESFactory.getFactory().getP7FileCommandInterface();
 		this.prova=0;
 	}
 
@@ -56,7 +66,6 @@ public final class FirmapiuDImpl implements FirmapiuDInterface {
 	 */
 	@Override
 	public boolean isRemote() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -69,72 +78,117 @@ public final class FirmapiuDImpl implements FirmapiuDInterface {
 		System.out.println("Stesso oggetto?? prova ->"+prova);
 		prova++;
 		
-		// TODO Auto-generated method stub
 		//FIXME da cambiare nel momento in cui si riscrive libreria
 		//linka l'implementazione concreta del demone alla libreria firmapiulib
 		//prepara i parametri da passare a firmapiulib
 		if (args==null || args.length==0)
 			throw new DBusExecutionException(localrb.getString("error0"));
-		DataFilePath dataFile= (DataFilePath)MasterFactoryBuilder.getFactory(DATAFACTORY).getData(DATAFILEPATH);
+		DataFile dataFile= DataFactoryBuilder.getFactory(DATAFILEFACTORY).getDataFile();
 		for(Variant<?> arg : args){
 			try {
-				dataFile.setData((String)arg.getValue());
+				File file = new File((String)arg.getValue());
+				dataFile.setData(file);
 			} catch (FirmapiuException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				throw new DBusExecutionException(localrb.getString("error0")+" : <"+e.errorCode+"> "+e.getLocalizedMessage());
 			}
 		}
-		
-		GenericArgument arguments=null;
+		//setta le opzioni
+		//GenericArgument arguments=null;
+		String pin=null;
 		if(options!=null){
-			arguments=(GenericArgument)MasterFactoryBuilder.getFactory(ARGUMENTFACTORY).getArgument(GENERICARGUMENT);
+			//arguments=(GenericArgument)MasterFactoryBuilder.getFactory(ARGUMENTFACTORY).getArgument(GENERICARGUMENT);
 			Iterator<String> itr=options.keySet().iterator();
 			while(itr.hasNext()){
 				//deve fare l'unmarshalling delle opzioni da quelle ricevute in ingresso a quelle richieste da sign
 				String key=itr.next();
-				Object value = unmarshall(key, options.get(key));
-				arguments.setArgument(key, value);
+				//il pin deve essere salvato a parte
+				//TODO controllare che questa rappresentazione vada bene
+				String value = unmarshall(options.get(key));
+				if (key.equals(TOKENPIN))
+					pin=value;
+				else
+					try {
+						dataFile.setArgument(key, value);
+					} catch (FirmapiuException e) {
+						e.printStackTrace();
+						throw new DBusExecutionException(localrb.getString("error3f")+" : "+e.getLocalizedMessage());
+					}
 			}
 		}
+		if(pin==null)
+			throw new DBusExecutionException(localrb.getString("error0p")+" : "+TOKENPIN);
 		
 		//overide del path dove si carica la libreria per leggere la carta
 		//commandoptions.put(CommandProxyInterface.DRIVERPATH, DPATH);
 		
 		//in caso di eccezione la rilancia come errore di dbus
-		Map<?,?> result;
+		//Crea l'interfaccia di comando e si logga sul token
+		ResultInterface<File,File> result=null;
+		//TODO da rivedere per ottimizzare tempo/memoria
+		PKCS11Token pkcs11Token = null;
 		try {
-			result=cadesBesInterface.sign(dataFile, arguments);
+			//crea il token per fare la firma 
+			pkcs11Token = TokenFactoryBuilder.getFactory(PKCS11TOKENFACTORY).getPKCS11Token(CRTSMARTCARD);
+			P7FileCommandInterface p7CommandInterface = CadesBESFactory.getFactory().getP7FileCommandInterface(pkcs11Token,null);
+			//si logga sul token, firma e si slogga
+			pkcs11Token.login(pin.toCharArray());
+			result=p7CommandInterface.sign(dataFile);
+			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block: da sistemare i log
 			e.printStackTrace();
 			throw new DBusExecutionException(localrb.getString("error3f")+" : "+e.getLocalizedMessage());
+		}finally{
+			try {
+				pkcs11Token.logout();
+			} catch (FirmapiuException e) {
+				e.printStackTrace();
+				throw new DBusExecutionException(localrb.getString("error3f"));
+			} catch (Exception e){}
 		}
 		
 		//effettua il marshalling dei risultati da inviare a dbus
 		Map<String,Variant<?>> dbusResult = new TreeMap<String,Variant<?>>();
-		Iterator<?> itr=result.keySet().iterator();
+		Iterator<File> itr;
+		try {
+			itr = result.getResultDataSet().iterator();
+		} catch (FirmapiuException e) {
+			e.printStackTrace();
+			throw new DBusExecutionException(localrb.getString("error3f")+" : "+e.getLocalizedMessage());
+		}
 		while(itr.hasNext()){
-			String key=(String)itr.next();
-			Object oldValue=result.get(key);
+			File keyFile=itr.next();
+			String key=keyFile.getAbsolutePath();
 			Variant<?> newValue;
-			if(oldValue instanceof String)
-			{
-				newValue=new Variant<Object>(oldValue,"s");
-			}
-			else if(oldValue instanceof FirmapiuException)
-			{
-				//FIXME da fissare con struct
-				//String str=oldValue.getClass().getCanonicalName()+" : "+((Exception)oldValue).getLocalizedMessage();
-				FirmapiuException fe1=(FirmapiuException) oldValue;
-				FirmapiuExceptionStruct struct = new FirmapiuExceptionStruct(fe1.errorCode,fe1.getLocalizedMessage());
+			try {
+				File oldValue=result.getResult(keyFile);
+				newValue=new Variant<String>(oldValue.getAbsolutePath(),"s");
+			} catch (FirmapiuException e) {
+				e.printStackTrace();
+				FirmapiuExceptionStruct struct = new FirmapiuExceptionStruct(e.errorCode,e.getLocalizedMessage());
 				newValue=new Variant<Object>(struct,"(is)");
-			}else
-				throw new DBusExecutionException(localrb.getString("error3f")+" : "+localrb.getString("error4f"));
+			}
 			dbusResult.put(key, newValue);
 		}//fine while
 		
 		return dbusResult;
+		
+//			if(oldValue instanceof String)
+//			{
+//				newValue=new Variant<Object>(oldValue,"s");
+//			}
+//			else if(oldValue instanceof FirmapiuException)
+//			{
+//				//String str=oldValue.getClass().getCanonicalName()+" : "+((Exception)oldValue).getLocalizedMessage();
+//				FirmapiuException fe1=(FirmapiuException) oldValue;
+//				FirmapiuExceptionStruct struct = new FirmapiuExceptionStruct(fe1.errorCode,fe1.getLocalizedMessage());
+//				newValue=new Variant<Object>(struct,"(is)");
+//			}else
+//				throw new DBusExecutionException(localrb.getString("error3f")+" : "+localrb.getString("error4f"));
+//			dbusResult.put(key, newValue);
+//		}//fine while
+		
+//		return dbusResult;
 	}
 
 	/* (non-Javadoc)
@@ -142,7 +196,6 @@ public final class FirmapiuDImpl implements FirmapiuDInterface {
 	 */
 	@Override
 	public Map<String,String> verify(Variant<?>[] args) {
-		// TODO Auto-generated method stub
 		//FIXME da cambiare quando si cambia libreria
 		//prepara i parametri da passare a firmapiulib
 		if (args==null || args.length==0)
@@ -182,22 +235,33 @@ public final class FirmapiuDImpl implements FirmapiuDInterface {
 	
 	//effettua l'unmarshalling delle opzioni ricevute in maniera tale da convertire il formato del valore delle opzioni
 	//ricevute da dbus in quello da passare alla libreria
-	private Object unmarshall(String key,Variant<?> value){
-		if(key.equals(CommandProxyInterface.PIN)){
-			//si aspetta che il valore passato abbia signature = "s" e lo trasforma in char[]
-			if(value.getSig().equals("s")){
-				return ((String)(value.getValue())).toCharArray();
-			}
-			else throw new DBusExecutionException(key+" : "+localrb.getString("error1"));
-		}else if(key.equals(CommandProxyInterface.OUTDIR)){
-			//si aspetta che il valore passato abbia signature = "s", non effettua trasformazioni
-			if(value.getSig().equals("s")){
-				return ((String)(value.getValue()));
-			}
-			else throw new DBusExecutionException(key+" : "+localrb.getString("error1"));
-		}
-		else
-			//se non c'è nessuna opzione valida lancia un errore
-			throw new DBusExecutionException(key+" : "+localrb.getString("error1"));
+//	private Object unmarshall(String key,Variant<?> value){
+//		if(key.equals(CommandProxyInterface.PIN)){
+//			//si aspetta che il valore passato abbia signature = "s" e lo trasforma in char[]
+//			if(value.getSig().equals("s")){
+//				return ((String)(value.getValue())).toCharArray();
+//			}
+//			else throw new DBusExecutionException(key+" : "+localrb.getString("error1"));
+//		}else if(key.equals(CommandProxyInterface.OUTDIR)){
+//			//si aspetta che il valore passato abbia signature = "s", non effettua trasformazioni
+//			if(value.getSig().equals("s")){
+//				return ((String)(value.getValue()));
+//			}
+//			else throw new DBusExecutionException(key+" : "+localrb.getString("error1"));
+//		}
+//		else
+//			//se non c'è nessuna opzione valida lancia un errore
+//			throw new DBusExecutionException(key+" : "+localrb.getString("error1"));
+//	}
+	
+	private String unmarshall(Variant<?> value){
+		//i tipi dei valori possono essere solo basic types (Stringhe e Boolean)
+		if(value.getSig().equals("s")){
+			return (String)(value.getValue());
+		}else if (value.getSig().equals("b")){
+			Boolean valBool = (Boolean)(value.getValue());
+			return valBool.toString();
+		}else
+			throw new DBusExecutionException(localrb.getString("error0"));	
 	}
 }
